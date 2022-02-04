9566b9e7d62294609c15c109e65bf988de4ed81c
Lauren Hess <laurenbethhess@gmail.com>
2022-02-04 12:14:03 -0500

    restyled some main page components

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import ReviewsCard from "./ReviewsCard";
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 
 function Home( { property, reviews } ) {
     const [homePhoto, setHomePhoto] = useState('')
@@ -25,14 +27,29 @@ function Home( { property, reviews } ) {
 
     return (
         <div>
-            <div className="welcome">
-                    Welcome to {property.name}!
+
+            <div align='center'>
+                <Box sx={{paddingTop: 12, width: '100%', maxWidth: 1200 }}>
+                    <Typography style={{fontWeight: "bold", fontSize: 30, fontFamily: "Courier"}} align='center' variant="p" gutterBottom component="div">
+                        Welcome to {property.name}!
+                    </Typography>
+                </Box>
             </div>
-            <div style={{paddingTop:25, display:'flex', justifyContent:'center', alignItems:'center'}}>
-                <img className="home_photo"  src={homePhoto.photo_url}/>
+
+            <div align='center'>
+                <Box sx={{paddingTop: 1, width: '100%', maxWidth: 1200 }}>
+                    <Typography>
+                        <img className="home_photo"  src={homePhoto.photo_url}/>
+                    </Typography>
+                </Box>
             </div>
-            <div className="description">
-                {property.description}
+            <br/>
+            <div align='center'>
+                <Box sx={{ width: '100%', maxWidth: 1200 }}>
+                    <Typography style={{fontSize: 20, fontFamily: "Courier"}} align='center' variant="p" gutterBottom component="div">
+                    {property.description}
+                    </Typography>
+                </Box>
             </div>
             <br/>
             {renderReviews}
